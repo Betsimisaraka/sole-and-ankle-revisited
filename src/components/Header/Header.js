86ce4d1de989cb648b8b9f21b3f633064edbc562
Betsimisaraka <anita.ras@onja.org>
2022-05-10 10:24:39 +0300

    Header alignment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -66,6 +66,7 @@ const MainHeader = styled.div`
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   border-top: 4px solid ${COLORS.gray[900]};
+  overflow: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     justify-content: space-between;
@@ -80,7 +81,7 @@ const MainHeader = styled.div`
 
 const Nav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
